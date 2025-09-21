a629f670aaf56ac6f7314062067fc75e42b3ef09
sazzadrupak <sazzad@oployeelabs.com>
2025-09-21 23:05:26 +0300

    first commit

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import SnippetEditForm from '@/components/snippet-edit-form';
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+interface SnippetEditPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const { id } = await props.params;
+
+  const snippetId = parseInt(id);
+  const snippet = await db.snippet.findFirst({
+    where: { id: snippetId },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
+}
